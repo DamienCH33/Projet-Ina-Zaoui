f7e7ba1d367eb6ff28e68fe657e9501270f5c04b
DamienCH33 <damienchauveau@hotmail.fr>
2026-02-04 11:21:28 +0100

    maj fixture + ajout factory

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setName('Ina Zaoui');
+        $admin->setEmail('ina@zaoui.com');
+        $admin->setPassword($this->hasher->hashPassword($admin, 'password'));
+        $admin->setAdmin(true);
+        $admin->setIsActive(true);
+        $manager->persist($admin);
+
+        $guest = new User();
+        $guest->setName('Invité 1');
+        $guest->setEmail('invite+1@example.com');
+        $guest->setPassword($this->hasher->hashPassword($guest, 'password'));
+        $guest->setAdmin(false);
+        $guest->setIsActive(true);
+        $manager->persist($guest);
+
+        $manager->flush();
+    }
+}
---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<User>
+ */
+final class UserFactory extends PersistentObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct() {}
+
+    #[\Override]
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'admin' => false,
+            'email' => self::faker()->unique()->safeEmail(),
+            'isActive' => true,
+            'name' => self::faker()->name(),
+            'password' => 'password',
+            'description' => self::faker()->optional()->text(200),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    #[\Override]
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function(User $user): void {
+                $user->setPassword(password_hash($user->getPassword(), PASSWORD_BCRYPT));
+            });
+    }
+}
